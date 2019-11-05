855130a8696f3fb5ac34de982a7d9b0709332c0e
Andreas Eberli <andreas@eberli.me>
2019-11-05 07:53:31 +0100

    Aktuelle Version

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #/bin/bash
-docker build --pull -t darkcheyenne/strongswan-cosmic_base:5.8.0 .
+docker build --pull -f Dockerfile.5.8.0 -t darkcheyenne/strongswan-cosmic_base:5.8.0 .
+docker build --pull -f Dockerfile.5.8.1 -t darkcheyenne/strongswan-cosmic_base:5.8.1 -t darkcheyenne/strongswan-cosmic_base:latest .
+
+docker push darkcheyenne/strongswan-cosmic_base:5.8.0 
+docker push darkcheyenne/strongswan-cosmic_base:5.8.1
+docker push darkcheyenne/strongswan-cosmic_base:latest
