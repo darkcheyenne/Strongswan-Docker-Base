8335994876af9c129fb08fdfbcd6127793172b90
Andreas Eberli <andreas@eberli.me>
2019-12-13 09:19:22 +0100

    Rückmeldung von Erfolg für Docker Image Bau Prozess an Syslog Server

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #/bin/bash
-docker build --pull -f Dockerfile.5.8.0 -t darkcheyenne/strongswan-cosmic_base:5.8.0 .
-docker build --pull -f Dockerfile.5.8.1 -t darkcheyenne/strongswan-cosmic_base:5.8.1 -t darkcheyenne/strongswan-cosmic_base:latest .
+logger Start von Erstellung von Docker Image Strongswan Base
+docker build --pull --no-cache -f Dockerfile.5.8.0 -t darkcheyenne/strongswan-cosmic_base:5.8.0 . | grep 'built\|tagged' | logger
+docker build --pull --no-cache -f Dockerfile.5.8.1 -t darkcheyenne/strongswan-cosmic_base:5.8.1 -t darkcheyenne/strongswan-cosmic_base:latest . | grep 'built\|tagged' | logger
 
+logger Upload von Strongswan Base zu Dockerhub
 docker push darkcheyenne/strongswan-cosmic_base:5.8.0 
 docker push darkcheyenne/strongswan-cosmic_base:5.8.1
 docker push darkcheyenne/strongswan-cosmic_base:latest
