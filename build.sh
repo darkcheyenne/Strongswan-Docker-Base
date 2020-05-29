28839ee67b24be9eb1a21126360e5b994ba57778
Andreas Eberli <andreas@eberli.me>
2020-05-29 07:07:25 +0000

    Update auf Ubuntu 20.04 und Strongswan 5.8.4

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #/bin/bash
-logger Start von Erstellung von Docker Image Strongswan Base
-docker build --pull --no-cache -f Dockerfile.5.8.0 -t darkcheyenne/strongswan-cosmic_base:5.8.0 . | grep 'built\|tagged' | logger
-docker build --pull --no-cache -f Dockerfile.5.8.1 -t darkcheyenne/strongswan-cosmic_base:5.8.1 -t darkcheyenne/strongswan-cosmic_base:latest . | grep 'built\|tagged' | logger
+echo 🚀 Start von Erstellung von Docker Image Strongswan Base
+echo "  🚀 Erstellen von Image fuer Strownswan 5.8.0"
+docker build --pull --no-cache -f Dockerfile.5.8.0 -t darkcheyenne/strongswan-cosmic_base:5.8.0 . | grep 'built\|tagged'
+echo "  🚀 Erstellen von Image fuer Strownswan 5.8.1"
+docker build --pull -f Dockerfile.5.8.1 -t darkcheyenne/strongswan-cosmic_base:5.8.1 . | grep 'built\|tagged'
+echo "  🚀 Erstellen von Image fuer Strownswan 5.8.4"
+docker build --pull -f Dockerfile.5.8.4 -t darkcheyenne/strongswan-cosmic_base:5.8.4 -t darkcheyenne/strongswan-cosmic_base:latest . | grep 'built\|tagged'
 
-logger Upload von Strongswan Base zu Dockerhub
+echo 📡 Upload von Strongswan Base zu Dockerhub
+echo "  📡 Upload von Image fuer Strownswan 5.8.0"
 docker push darkcheyenne/strongswan-cosmic_base:5.8.0 
+echo "  📡 Upload von Image fuer Strownswan 5.8.1"
 docker push darkcheyenne/strongswan-cosmic_base:5.8.1
+echo "  📡 Upload von Image fuer Strownswan 5.8.4"
+docker push darkcheyenne/strongswan-cosmic_base:5.8.4
+echo "  📡 Upload von Image fuer Strownswan latest"
 docker push darkcheyenne/strongswan-cosmic_base:latest
